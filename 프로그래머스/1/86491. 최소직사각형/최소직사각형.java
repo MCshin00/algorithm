579ebc3715b13eb40246209388ae2202ff1aa8e5
MCshin00 <105781379+MCshin00@users.noreply.github.com>
2024-11-23 22:27:16 +0900

    [level 1] Title: 최소직사각형, Time: 1.65 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxWidth = 0;
+        int maxHeight = 0;
+        
+        for(int i=0; i<sizes.length; i++) {
+            if (sizes[i][0] < sizes [i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            if (sizes[i][0] > maxWidth) maxWidth = sizes[i][0];
+            if (sizes[i][1] > maxHeight) maxHeight = sizes[i][1];
+        }
+        answer = maxWidth * maxHeight;
+        return answer;
+    }
+}
