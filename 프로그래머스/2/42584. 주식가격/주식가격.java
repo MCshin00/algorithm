92746e5982406831468bded8731427fbfbf6997a
MCshin00 <105781379+MCshin00@users.noreply.github.com>
2025-02-26 19:13:49 +0900

    [level 2] Title: 주식가격, Time: 71.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> s = new Stack<>();
+        
+        for(int i=0; i<prices.length; i++) {
+            while(!s.isEmpty() && prices[i] < prices[s.peek()]) {
+                answer[s.peek()] = i - s.peek();
+                s.pop();
+            }
+            
+            s.push(i);
+        }
+        
+        while(!s.isEmpty()) {
+            answer[s.peek()] = prices.length - s.peek() - 1;
+            s.pop();
+        }
+        
+        return answer;
+    }
+}
